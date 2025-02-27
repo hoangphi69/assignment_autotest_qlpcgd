d3eac961d96cf58fe512c2207e31bb1f71b85108
Ex-Teni <tienteni01@gmail.com>
2025-02-27 21:21:05 +0700

    Đã note tc trong list

--- src/test/java/MajorManagement/F0401_ListMajors.java
+++ src/test/java/MajorManagement/F0401_ListMajors.java
@@ -12,59 +12,59 @@ public class F0401_ListMajors extends MajorPage {
 
   public void performListMajors() {
 
-    // Hiển thị 25 trang dữ liệu
+    // TC01: Hiển thị 25 trang dữ liệu
     delay(2000);
     WebElement numberOfList = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/div/section/div/div/div/div[2]/div/div/div[1]/div[1]/div/label/select"));
     numberOfList.click();
     WebElement ten25List = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/div/section/div/div/div/div[2]/div/div/div[1]/div[1]/div/label/select/option[2]"));
     ten25List.click();
 
-    //page roll down
-    delay(1000);
-    ((org.openqa.selenium.JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+      //page roll down
+      delay(1000);
+      ((org.openqa.selenium.JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
 
-    // Chuyển qua trang tiếp theo
+    // TC02: Chuyển qua trang tiếp theo
     delay(4000);
     WebElement nextList = driver.findElement(By.id("tblMajor_next"));
     nextList.click();
 
-    // Chuyển về trang trước đó
+    // TC03: Chuyển về trang trước đó
     delay(4000);
     WebElement prevList = driver.findElement(By.id("tblMajor_previous"));
     prevList.click();
 
-    //page roll up
+    // TC04: Thử button lướt lên đầu trang
     delay(2000);
     WebElement pageRollUp = driver.findElement(By.xpath("/html/body/div[2]/button"));
     pageRollUp.click();
 
-    // Hiển thị 50 trang dữ liệu
+    // TC05: Hiển thị 50 trang dữ liệu
     delay(4000);
     numberOfList.click();
     WebElement ten50List = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/div/section/div/div/div/div[2]/div/div/div[1]/div[1]/div/label/select/option[3]"));
     ten50List.click();
 
-    //page roll down
-    delay(1000);
-    ((org.openqa.selenium.JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-    
-    //page roll up
-    delay(2000);
-    pageRollUp.click();
+      //page roll down
+      delay(1000);
+      ((org.openqa.selenium.JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+      //page roll up
+      delay(2000);
+      pageRollUp.click();
 
 
-    // Hiển thị tất cả trang dữ liệu
+    // TC06: Hiển thị tất cả trang dữ liệu
     delay(4000);
     numberOfList.click();
     WebElement tenAllList = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/div/section/div/div/div/div[2]/div/div/div[1]/div[1]/div/label/select/option[4]"));
     tenAllList.click();
 
-    //page roll down
-    delay(1000);
-    ((org.openqa.selenium.JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-    
-    //page roll up
-    delay(2000);
-    pageRollUp.click();
+      //page roll down
+      delay(1000);
+      ((org.openqa.selenium.JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+      
+      //page roll up
+      delay(2000);
+      pageRollUp.click();
   }
 }
